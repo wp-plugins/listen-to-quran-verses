20a2fbb3c1dcc5ae6e8316eb6407fc4dcb49d40e
ibralnet <ibralnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-13 22:23:49 +0000

    update version of the PHP file
    git-svn-id: https://plugins.svn.wordpress.org/listen-to-quran-verses/trunk@612079 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mqv.php
+++ mqv.php
@@ -3,8 +3,8 @@
 Plugin Name: Listen to Quran Verses
 Plugin URI: http://listenquran.wadakkir.org/
 Description: This plugin let's you easily add Quran to your website with a flexible way to help people memorize its verses, and a very simple, easy to integrate within the current theme, and fully customizable interface supporting LTR as well as RTL design form. demo on http://www.wadakkir.org/
-Version: 1.0
-Author: Larbi Abu Romaïsae
+Version: 1.1
+Author: Larbi Abu Roma√Øssae
 Author URI: http://aburomaissae.wadakkir.org
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
